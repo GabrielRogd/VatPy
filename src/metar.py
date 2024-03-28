6382de22f92db418ba4af4b636eea8860f106d4c
GabrielRogd <gabrielrogd@protonmail.ch>
2024-03-28 16:06:52 +0200

    Refined QNH decoding - once more :(
    Thank god this is a development branch...

--- src/metar.py
+++ src/metar.py
@@ -80,8 +80,10 @@ def decode_METAR(METAR):
 
         if item.startswith('Q'):  # QNH decoding
             if item[1] == "0":
-                item=item[2:]
-            baro = f"QNH: {item[1:]}hpa"
+                item = item[2:]
+                baro = f"QNH: {item}hpa"
+            elif item[1] == "1":
+                baro = f"QNH: {item[1:]}hpa"
             
 
         elif item.startswith('A2') or item.startswith('A3'):  # Altimeter decoding
